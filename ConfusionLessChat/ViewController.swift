eaa124333e19256e78bb768934bdb5a1bb55bd9e
Rahul Bhalla <rahulbhalla17@gmail.com>
2019-05-03 18:32:49 +0530

    Testing 2

--- ConfusionLessChat/ViewController.swift
+++ ConfusionLessChat/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         print("Test")
+        print("Test2")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
